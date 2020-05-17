ba558c90b59b16cc09f455adca6f030e8ed62a01
satish <android24n@gmail.com>
2020-05-17 23:58:06 +0530

    first commit

---
+++ conf.js
@@ -0,0 +1,10 @@
+exports.config = {
+  framework: 'jasmine',
+  seleniumAddress: 'http://localhost:4444/wd/hub',
+  specs: ['spec.js']
+}
+
+<
+this is all rough..
+these are the new lines added..
+>
